5397fe84104420a89945b6fca7da3e5ea963e7af
knyg <kevin1992yang@gmail.com>
2020-10-29 22:11:49 -0700

    starter files: react, sass, and initial routing

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './MAIN.scss';
+
+import Card from './Components/card/card';
+
+function App() {
+  return (
+    <div className="App">
+      <Card/>
+    </div>
+  );
+}
+
+export default App;
